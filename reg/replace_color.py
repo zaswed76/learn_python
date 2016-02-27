5bd2c877b2be177e44663ef6f6ad904a2086a9f8
sergk <zaswed76@gmail.com>
2016-02-27 20:53:57 +0200

    111

--- reg/replace_color.py
+++ reg/replace_color.py
@@ -3,3 +3,19 @@
 
 
 import re
+
+s = '''button {
+    background: grey;
+    border-color gray;
+}'''
+
+pat = re.compile('''
+(button\s+\{)
+(.*)
+(border-color\s+)
+(\w+)
+(\W*)
+
+''', re.VERBOSE | re.DOTALL | re.IGNORECASE)
+
+print(pat.findall(s))
